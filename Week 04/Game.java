9e0af2ffdd497d5c2abd282b708c8aa9fa70d9d2
MooseValley <Moose_Software@yahoo.com.au>
2021-03-31 11:14:40 +1000

    Week 04
    Week 04

---
+++ Week 04/Game.java
@@ -0,0 +1,38 @@
+enum ERROR_CODES {PRINTER_ERROR, DISK_ERROR, NETWORK_ERROR};
+
+
+
+enum GAME_STATUS {GAME_OVER, IN_PROGRESS, HUMAN_WINS, DRAW};
+/*
+{
+	public GAME_STATUS ()
+	{
+	}
+
+	// Acessor
+
+
+	// Mutator
+
+}
+
+*/
+
+public class Game
+{
+	//int gameStatus = 0;
+
+	GAME_STATUS gamesStatus = GAME_STATUS.GAME_OVER;
+
+
+	//Customer mike = new Customer ();
+
+
+	gamesStatus = GAME_STATUS.HUMAN_WINS;
+
+	if (gamesStatus == GAME_STATUS.IN_PROGRESS)
+	{
+		klajklajdl
+	}
+
+}
