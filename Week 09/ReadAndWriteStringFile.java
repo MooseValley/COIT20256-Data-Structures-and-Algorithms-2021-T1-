bcba998a654f02af8e5902209f91e133c0bca5fb
MooseValley <Moose_Software@yahoo.com.au>
2021-05-19 15:09:22 +1000

    Week 09
    Week 09 tutorial work

--- Week 09/ReadAndWriteStringFile.java
+++ Week 09/ReadAndWriteStringFile.java
@@ -1,3 +1,9 @@
+/*
+ Author:  Mike O'Malley
+   Date:  11-May-2021
+Descrtn:  Week 09 Tutorial questions - File I/O
+
+*/
 import java.nio.file.Paths;
 import java.nio.file.Path;
 import java.io.IOException;
@@ -8,58 +14,58 @@ import java.util.*;
 
 public class ReadAndWriteStringFile
 {
-	public static void readFile (String fileNameStr)
-	{
-		try
-		{
-			Scanner sc = new Scanner (Paths.get(fileNameStr) );
+   public static void readFile (String fileNameStr)
+   {
+      try
+      {
+         Scanner sc = new Scanner (Paths.get(fileNameStr) );
 
-			//Open it
+         //Open it
 
-			while (sc.hasNext() == true)
-			{
-				System.out.println (sc.nextLine() );
-			}
+         while (sc.hasNext() == true)
+         {
+            System.out.println (sc.nextLine() );
+         }
 
-			// Close it
-			sc.close();
+         // Close it
+         sc.close();
 
-		}
-		catch (Exception err)
-		{
-			System.out.println ("ERROR: file '" + fileNameStr + "' does not exist.");
-		}
-	}
+      }
+      catch (Exception err)
+      {
+         System.out.println ("ERROR: file '" + fileNameStr + "' does not exist.");
+      }
+   }
 
-	public static void writeFile (String fileNameStr, String[] strArray)
-	{
-		try
-		{
-			Formatter outFile = new Formatter (fileNameStr);
+   public static void writeFile (String fileNameStr, String[] strArray)
+   {
+      try
+      {
+         Formatter outFile = new Formatter (fileNameStr);
 
-			for (int k = 0; k < strArray.length; k++)
-			{
-				outFile.format ("%s%n", strArray [k]);
-			}
+         for (int k = 0; k < strArray.length; k++)
+         {
+            outFile.format ("%s%n", strArray [k]);
+         }
 
-			outFile.close();
-		}
-		catch (Exception err)
-		{
-			System.out.println ("ERROR: file '" + fileNameStr + "' does not exist.");
-		}
-	}
+         outFile.close();
+      }
+      catch (Exception err)
+      {
+         System.out.println ("ERROR: file '" + fileNameStr + "' does not exist.");
+      }
+   }
 
 
-	public static void main(String[] args)
-	{
+   public static void main(String[] args)
+   {
 
-		String[] StrArray = {"Mike", "Iftekhar", "Frankie", "Bella"};
+      String[] StrArray = {"Mike", "Iftekhar", "Frankie", "Bella"};
 
-		//writeFile ("out.dat", StrArray );
+      //writeFile ("out.dat", StrArray );
 
-		//readFile ("ReadAndWriteStringFile.java");
+      //readFile ("ReadAndWriteStringFile.java");
 
-		readFile ("out2.dat");
-	}
+      readFile ("out2.dat");
+   }
 }
